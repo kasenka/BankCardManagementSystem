7240f54f67800157b9eef2f2c4aeec0770c5a68e
kasenka <kosenkoda37@mail.ru>
2025-09-29 11:25:02 +0300

    создание AuthController с эндпоинтами: /login, /register, /refresh, /logout

---
+++ src/main/java/com/example/bankcards/controller/AuthController.java
@@ -0,0 +1,140 @@
+package com.example.bankcards.controller;
+
+import com.example.bankcards.dto.TokenRefreshDTO;
+import com.example.bankcards.dto.UserAuthDTO;
+import com.example.bankcards.dto.UserDTO;
+import com.example.bankcards.entity.Role;
+import com.example.bankcards.entity.User;
+import com.example.bankcards.repository.RefreshTokenRepository;
+import com.example.bankcards.repository.UserRepository;
+import com.example.bankcards.service.JwtService;
+import com.example.bankcards.service.RefreshTokenService;
+import com.example.bankcards.util.UserMapper;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("api/auth")
+public class AuthController {
+
+    private UserRepository userRepository;
+    private AuthenticationManager authenticationManager;
+    private JwtService jwtService;
+    private UserMapper userMapper;
+    private PasswordEncoder passwordEncoder;
+    private RefreshTokenService refreshTokenService;
+    private RefreshTokenRepository refreshTokenRepository;
+
+
+    public AuthController(UserRepository userRepository,
+                          AuthenticationManager authenticationManager,
+                          JwtService jwtService,
+                          UserMapper userMapper,
+                          PasswordEncoder passwordEncoder,
+                          RefreshTokenService refreshTokenService,
+                          RefreshTokenRepository refreshTokenRepository) {
+        this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+        this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
+        this.refreshTokenService = refreshTokenService;
+        this.refreshTokenRepository = refreshTokenRepository;
+    }
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<?> register(@RequestBody @Valid UserAuthDTO userAuthDTO,
+                                      BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<String> errors = bindingResult.getAllErrors().stream()
+                    .map(error -> error.getDefaultMessage())
+                    .collect(Collectors.toList());
+            return ResponseEntity.badRequest()
+                    .body(Map.of("errors", errors));
+        }
+
+        if(userRepository.findByUsername(userAuthDTO.getUsername()).isPresent()){
+            return ResponseEntity.badRequest()
+                    .body(Map.of("error", "Этот юзернейм уже занят"));
+        }
+
+        User user = userMapper.map(userAuthDTO);
+
+        user.setEncryptedPassword(passwordEncoder.encode(userAuthDTO.getPassword()));
+        user.setRole(Role.USER);
+
+        userRepository.save(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userMapper.map(user));
+    }
+
+    @PostMapping(value = "/login")
+    public ResponseEntity<?> login(@RequestBody UserAuthDTO userAuthDTO){
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            userAuthDTO.getUsername(),
+                            userAuthDTO.getPassword()
+                    )
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            User user = userRepository.findByUsername(userAuthDTO.getUsername()).get();
+
+            String jwtAccess = jwtService.generateAccessToken(user);
+            String jwtRefresh = jwtService.generateRefreshToken(user);
+
+            refreshTokenService.saveRefreshToken(jwtRefresh, user.getUsername(),
+                    LocalDateTime.now().plusSeconds(jwtService.jwtExpirationRefresh));
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(Map.of("jwtAccess", jwtAccess,
+                            "jwtRefresh", jwtRefresh));
+        } catch (BadCredentialsException ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("error", "Неверный юзернейм или пароль"));
+        }
+    }
+
+    @PostMapping(value = "/refresh")
+    public ResponseEntity<?> refresh(@RequestBody TokenRefreshDTO tokenRefreshDTO){
+        if (refreshTokenService.isValid(tokenRefreshDTO.getRefreshToken())){
+            String username = refreshTokenService.getUsernameByToken(tokenRefreshDTO.getRefreshToken());
+            User user = userRepository.findByUsername(username).get();
+
+            String accessToken = jwtService.generateAccessToken(user);
+            return ResponseEntity.ok()
+                    .body(Map.of("jwtAccess", accessToken));
+        }return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Map.of("error", "Невалидный refreshToken"));
+    }
+
+    @DeleteMapping("/logout")
+    public ResponseEntity<?> logout(@RequestBody TokenRefreshDTO tokenRefreshDTO) {
+        if (refreshTokenRepository.findByToken(tokenRefreshDTO.getRefreshToken()).isEmpty()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(Map.of("error", "Refresh токен не валидный"));
+        }
+        refreshTokenService.deleteRefreshToken(tokenRefreshDTO.getRefreshToken());
+        return ResponseEntity.ok("Logged out");
+    }
+
+}
